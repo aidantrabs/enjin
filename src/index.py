093863d0d4a4bfb078ace6a5e5c90981cc173c34
Mobina Toorani <mobinatoorani@gmail.com>
2023-04-29 22:20:25 -0400

    Update inverted index

--- src/index.py
+++ src/index.py
@@ -1,51 +1,50 @@
 from typing import List
 import re
 from collections import defaultdict
-from collections import defaultdict
+from collections import defaultdict as dd
 import glob
 from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
+from nltk.corpus import stopwords as sw
 
-def tokenize(text: str) -> List[str]:
+def tokenize_text(text: str) -> List[str]:
     """
     Tokenize the given text.
     """
-    words = word_tokenize(text)
-    words = [word.lower() for word in words if word.isalnum()]
-    stop_words = set(stopwords.words('english'))
-    words = [word for word in words if word not in stop_words]
-    return words
+    word_list = word_tokenize(text)
+    filtered_words = []
+    for word in word_list:
+        if word.isalnum():
+            filtered_words.append(word.lower())
+    stop_words = set(sw.words('english'))
+    filtered_words_list = []
+    for word in word_list:
+        if word not in stop_words:
+            filtered_words_list.append(word)
+    return filtered_words_list
 
-def get_soundex(word):
+def get_soundex_code(word):
     """
     Get the soundex code for the given word.
     """
-    
-    # remove all non-alphabetic characters and convert to uppercase
     word = re.sub(r'[^A-Za-z]+', '', word.upper())
     
-    # handle empty string or strings with only one character
     if not word:
         return word
     
-    # remove select characters
-    removeCode = str.maketrans('', '', 'AEIOUYHW')
-    wordCode = word[1:].translate(removeCode)
+    remove_code = str.maketrans('', '', 'AEIOUYHW')
+    word_code = word[1:].translate(remove_code)
 
-    # map the first character to itself and the rest to their corresponding digits
     soundex_code = word[0]
     digit_map = str.maketrans('BFPVCGJKQSXZDTLMNR', '111122222222334556')
     soundex_code += word[1:].translate(digit_map)
     
-    # remove consecutive duplicates and all zeros except the first one
     soundex_code = re.sub(r'(\d)\1+', r'\1', soundex_code)
     soundex_code = re.sub(r'0', '', soundex_code)
     
-    # pad the code with zeros or truncate it to length 4
     soundex_code = soundex_code + '000'
     return soundex_code[:4]
 
-def inverted_index():
+def create_inverted_index():
     """
     Create inverted index and save it to file.
     """
@@ -57,10 +56,10 @@ def inverted_index():
         files = glob.glob(f"data/{topic}/*.txt")
         for file_path in files:
             file_hash = file_path.split("/")[-1].split(".")[0]
-            with open(file_path, "r", encoding="utf-8") as f:
-                content = f.read()
-                tokens = tokenize(content)
-                term_freq = defaultdict(int)
+            with open(file_path, "r", encoding="utf-8") as file:
+                content = file.read()
+                tokens = tokenize_text(content)
+                term_freq = dd(int)
                 for token in tokens:
                     term_freq[token] += 1
 
@@ -69,15 +68,14 @@ def inverted_index():
 
             mapping[file_hash] = doc_id
             doc_id += 1
-    with open("invertedindex.txt", "w", encoding="utf-8") as f:
+    with open("invertedindex.txt", "w", encoding="utf-8") as file:
         for term, appearances in inverted_index.items():
-            soundex = get_soundex(term)
+            soundex = get_soundex_code(term)
             appearances_str = ', '.join([f'({doc_id}, {freq})' for doc_id, freq in appearances])
             output_str = f"| {term} | {soundex} | {appearances_str} |\n"
-            f.write(output_str)
-    with open("mapping.txt", "w") as f:
-        print("Saving mapping...")
+            file.write(output_str)
+    with open("mapping.txt", "w") as file:
         for file_hash, doc_id in mapping.items():
-            f.write(f"{file_hash} {doc_id}\n")    
+            file.write(f"{file_hash} {doc_id}\n")    
             print()
-    print("Inverted index created and saved to file.")
+    print("Inverted index created and saved to file.")    
